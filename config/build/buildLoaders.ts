e23676b5496c2b67ed8a07b2b64b65b05ac1e780
rudgedev <nekcit98@gmail.com>
2023-05-08 12:08:15 +0300

    add router, add theme, add prettier and webpack settings

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,30 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { IBuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-    };
+export function buildLoaders({ isDev }: IBuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes('.module.'),
+            localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node-modules/,
-    };
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node-modules/,
+  };
 
-    return [typescriptLoader, cssLoader];
+  return [typescriptLoader, cssLoader];
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
-import './Counter.scss';
+import styles from './Counter.module.scss';
 
 export const Counter = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button className={styles.btn} onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
 };
