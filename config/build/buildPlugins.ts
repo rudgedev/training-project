255a3d32672e6a4afd9ac011e63b077e52db3eba
rudgedev <nekcit98@gmail.com>
2023-05-18 20:39:26 +0300

    add router config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,17 +3,15 @@ import webpack from 'webpack';
 import { IBuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-export function buildPlugins({
-    paths,
-}: IBuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-    ];
+export function buildPlugins({ paths }: IBuildOptions): webpack.WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+  ];
 }
